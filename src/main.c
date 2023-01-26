d64dfea1bbb7bc41f724dc2440c3d86b3eb58133
Ziliotti, Guido <guido.ziliotti@oracle.com>
2023-01-26 10:26:05 +0100

    esercizio 2

--- src/main.c
+++ src/main.c
@@ -1,20 +1,34 @@
+#include "stdbool.h"
 #include "stdio.h"
 #include "string.h"
 
-int paragona(char *t1, char *t2) {
-  for (char *p = t1, *q = t2; *p && *q; ++p, ++q) {
-    if ((*p) + 1 == *q)
-      continue;
-    return 0;
-  }
-  return 1;
-}
+bool check(char *v1, char *v2, int dim1, int dim2);
+bool cerca(char c, char *v2, int dim2);
 
 int main() {
-  char *t1 = "abc";
+  char *t1 = "aybc";
   char *t2 = "xbcd";
 
-  int ok = paragona(t1, t2);
+  bool ok = check(t1, t2, strlen(t1), strlen(t2));
 
   printf("risultato %d, %s - %s", ok, t1, t2);
+}
+
+bool check(char *v1, char *v2, int dim1, int dim2) {
+  for (int j = 0; j < dim1; ++j) {
+    if (cerca(v1[j] + 1, v2, dim2)) {
+      continue;
+    }
+    return false;
+  }
+
+  return true;
+}
+
+bool cerca(char c, char *testo, int dim) {
+  for (int j = 0; j < dim; ++j) {
+    if (testo[j] == c)
+      return true;
+  }
+  return false;
 }
